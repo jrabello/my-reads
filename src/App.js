9614dc475c75960d9d1655d38c03b9721f773631
Joilson Rabelo <joilson.rabello@gmail.com>
2018-08-19 03:39:27 -0300

    Adds new view-section component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
-// import * as BooksAPI from './BooksAPI'
 import './App.css'
-import BookCard from './BookCard';
+import BookViewerSection from './book/BookViewerSection';
 
 class BooksApp extends React.Component {
   state = {
@@ -46,52 +45,15 @@ class BooksApp extends React.Component {
             <div className="list-books-title">
               <h1>MyReads</h1>
             </div>
+
             <div className="list-books-content">
               <div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Currently Reading</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                       <BookCard/>
-                      </li>
-                      <li>
-                        <BookCard/>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Want to Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                        <BookCard/>
-                      </li>
-                      <li>
-                        <BookCard/>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
-                <div className="bookshelf">
-                  <h2 className="bookshelf-title">Read</h2>
-                  <div className="bookshelf-books">
-                    <ol className="books-grid">
-                      <li>
-                      <BookCard/>
-                      </li>
-                      <li>
-                      <BookCard/>
-                      </li>
-                      <li>
-                      <BookCard/>
-                      </li>
-                    </ol>
-                  </div>
-                </div>
+                <BookViewerSection title="Currently Reading"/>
+                <BookViewerSection title="Want to Read"/>
+                <BookViewerSection title="Read"/>
               </div>
             </div>
+
             <div className="open-search">
               <a 
                 onClick={() => this.setState({ showSearchPage: true })}>
---
+++ src/book/BookViewerSection.js
@@ -0,0 +1,37 @@
+import React from "react";
+import BookCard from "./BookCard";
+
+class BookViewerSection extends React.Component {
+  render() {
+    return (
+      <div className="bookshelf">
+      {/* TODO: add title as a parameter */}
+        <h2 className="bookshelf-title">{ this.props.title }</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            <li>
+              <BookCard book={
+                {
+                  imgUrl: `http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api`,
+                  title: 'The Linux Command Line',
+                  authors: ["William E. Shotts, Jr."],
+                }
+              }/>
+            </li>
+            <li>
+              <BookCard book={
+                {
+                  imgUrl: `http://books.google.com/books/content?id=nggnmAEACAAJ&printsec=frontcover&img=1&zoom=5&source=gbs_api`,
+                  title: 'The Linux Command Line',
+                  authors: ["William E. Shotts, Jr."],
+                }
+              }/>
+            </li>
+          </ol>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default BookViewerSection;
